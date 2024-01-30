9b2cf9ee050e37827373cf594119df47678598bb
commitEvil <thegen.jackson@gmail.com>
2024-01-30 19:27:25 +1000

    3rd commit
    - fixed issues with condition IF statements not exiting the functions
    - commented where relevant to tidy up what was messy before and added helpful reminders
    - began adapting the program to use tkinter for GUI but need alot more testing and reading docs

--- SimplePasswordManager.py
+++ SimplePasswordManager.py
@@ -1,9 +1,13 @@
+# Import tkinter - read more docs
+import tkinter as tk
+
 # Import Python Cryptography library and Fernet module according to documentation
 import cryptography
 from cryptography.fernet import Fernet
 
 
-# Required for encryption with Fernet as per documentation
+#Generate the Fernet encryption key
+# Required for encryption and decryption with Fernet as per documentation
 key = Fernet.generate_key()
 fernet = Fernet(key)
 
@@ -36,13 +40,17 @@ def add_pw():
     new_acct = str(input("Account: "))
     new_username = str(input("Username: "))
     new_pw = str(input("Password: "))
+    # User input value for new_pw is encoded then saved to a new variable
     encoded_pw = fernet.encrypt(new_pw.encode())
+    # The variable needs var.decode() when adding to the encrypted passwords list
+    # Otherwise it saves to the list as b'var' instead of 'var'
+    # Decode is different to Decrypt, remember to read the docs more
     list_pw.append([new_acct, new_username, encoded_pw.decode()])
 
     again = str(input("Add another password?\nType Y for Yes or N for No\n(y/n)\n"))
-    if again == "Y" or again == "y":
+    if again.lower() == "y":
         add_pw()
-    elif again == "N" or again == "n":
+    elif again.lower() == "n":
         start()
 
 
@@ -52,14 +60,19 @@ def edit_pw():
     print(sorted_list)
     index = int(input("Select a password to edit by typing the index: "))
     replace_pw = str(input("New Password: "))
+    # User input value for replace_pw is encoded then saved to a new variable
     replace_encoded_pw = fernet.encrypt(replace_pw.encode())
+    # Issues arise when assigning the value directly to the sub-list index
+    # To avoid this we first remove the last value in the relevant sub-list
+    # Then append the decoded variable to the relevant sub-list
+    # Remember the value needs to be decoded before adding to the encrypted passwords list
     list_pw[index].remove(list_pw[index][-1])
     list_pw[index].append(replace_encoded_pw.decode())
 
     again = str(input("Edit another password?\nType Y for Yes or N for No\n(y/n)\n"))
-    if again == "Y" or again == "y":
+    if again.lower() == "y":
         edit_pw()
-    elif again == "N" or again == "n":
+    elif again.lower() == "n":
         start()
 
 
@@ -71,9 +84,9 @@ def del_pw():
     list_pw.remove(list_pw[index])
 
     again = str(input("Remove another password?\nType Y for Yes or N for No\n(y/n)\n"))
-    if again == "Y" or again == "y":
+    if again.lower() == "y":
         del_pw()
-    elif again == "N" or again == "n":
+    elif again.lower() == "n":
         start()
 
 
@@ -82,15 +95,23 @@ def show_pw():
     sorted_list = sorted(list_pw)
     print(sorted_list)
     index = int(input("Select a password to decrypt by typing the index: "))
+    # Similar to encrypting, the decrypted password needs to be stored in a new variable
     decoded_pw = fernet.decrypt(list_pw[index][-1])
+    # Remember to decode the new variable to remove the leading b value
+    # This changes b'variable' to 'variable'
     print(decoded_pw.decode())
 
     again = str(input("Decrypt another password?\nType Y for Yes or N for No\n(y/n)\n"))
-    if again == "Y" or again == "y":
+    if again.lower() == "y":
         show_pw()
-    elif again == "N" or again == "n":
+    elif again.lower() == "n":
         start()
 
 
-# Start the program
 start()
+# # Create the root frame window thing with tkinter and initialize the app - read more docs
+# root = tk.Tk()
+# root.title("Simple Password Manager")
+
+# # Run the app
+# root.mainloop()
